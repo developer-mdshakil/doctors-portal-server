4c27c05bde86f4faba5dbe02262c6339d6b530e2
developer-mdshakil <developerdevelopershakil.cmt1819@gmail.com>
2022-11-18 10:17:55 +0600

    get data from monogodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require ('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,6 +14,25 @@ app.get('/', (req, res) => {
     res.send('Hello patieant come with me!')
 })
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.io31lql.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+    const apointmentOpstionCollection = client.db('doctors-portal').collection('apointmentoption')
+    app.get('/bookingoptions', async(req, res) => {
+    const query = {};
+    const bookingOpions = await apointmentOpstionCollection.find(query).toArray()
+    res.send(bookingOpions)
+    })
+    }
+    finally{
+
+    }
+}
+run().catch(erorr => console.log(erorr))
 app.listen(port, () => {
     console.log(`Doctors portal server running ${port}`)
 })
