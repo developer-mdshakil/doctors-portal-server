76c02deb39f700e25a766804734908991ce2c793
developer-mdshakil <developerdevelopershakil.cmt1819@gmail.com>
2022-11-22 16:42:38 +0600

    check valid admin promoter

--- index.js
+++ index.js
@@ -77,14 +77,25 @@ async function run(){
         res.send(result);
         
     })
-
+    app.get('/users/admin/:email', async(req, res)=> {
+        const email = req.params.email;
+        const query = { email };
+        const user = await usersCollection.findOne(query);
+        res.send({ isAdmin: user?.role === 'admin'})
+    })
     app.get('/users', async(req, res)=> {
         const query = {};
         const users = await usersCollection.find(query).toArray();
         res.send(users)
     })
 
-    app.put('/users/admin/:id', async(req, res)=> {
+    app.put('/users/admin/:id', verifyJWT, async(req, res)=> {
+        const decodedEmail = req.decoded.email;
+        const query = {email: decodedEmail};
+        const user = await usersCollection.findOne(query);
+        if(user?.role !== 'admin'){
+            return res.status(403).send({message: 'forbidend access'})
+        }
         const id = req.params.id;
         const filter = {_id: ObjectId(id)};
         const options = { upsert: true };
